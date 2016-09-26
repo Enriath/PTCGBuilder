41421a4c94c4a14544313ef340dabf30106cae0e
Hydrox6 <ikada@protonmail.ch>
2016-09-26 17:15:17 +0100

    Added an external library of useful code from the internet
    It's self made with snippets from across the internet (currently only the AutoScrollbar from effbot.org)

---
+++ external.py
@@ -0,0 +1,21 @@
+##A place to store external code that I find from third parties.
+from tkinter import *
+
+
+##Credit: Fredrik Lundh
+##Date	: 08/09/1998
+##URL	: http://effbot.org/zone/tkinter-autoscrollbar.htm
+class AutoScrollbar(Scrollbar):
+    # A scrollbar that hides itself if it's not needed.
+    # Only works if you use the grid geometry manager!
+    def set(self, lo, hi):
+        if float(lo) <= 0.0 and float(hi) >= 1.0:
+            # grid_remove is currently missing from Tkinter!
+            self.tk.call("grid", "remove", self)
+        else:
+            self.grid()
+        Scrollbar.set(self, lo, hi)
+    def pack(self, **kw):
+        raise TclError("cannot use pack with this widget")
+    def place(self, **kw):
+        raise TclError("cannot use place with this widget")
